210c07bde7bb4913987b47d5c19a24df1cb0e24d
Jens Rydholm <spacejens@gmail.com>
2010-11-11 15:10:32 +0100

    Logging to gagror.log in Glassfish domain logs directory.

--- src/main/java/se/spacejens/gagror/spring/WelcomeController.java
+++ src/main/java/se/spacejens/gagror/spring/WelcomeController.java
@@ -3,6 +3,8 @@ package se.spacejens.gagror.spring;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -13,9 +15,13 @@ import org.springframework.web.servlet.mvc.Controller;
  */
 public class WelcomeController implements Controller {
 
+	private static final Logger log = LoggerFactory
+			.getLogger(WelcomeController.class);
+
 	@Override
 	public ModelAndView handleRequest(final HttpServletRequest arg0,
 			final HttpServletResponse arg1) {
+		WelcomeController.log.info("Handling request");
 		return new ModelAndView("welcome");
 	}
 }
