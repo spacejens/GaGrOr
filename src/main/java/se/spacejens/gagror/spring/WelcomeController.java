2ba0342e9b387c69babe5475f5e5a65ff06e573a
Jens Rydholm <spacejens@gmail.com>
2010-11-15 14:43:24 +0100

    Moved persistence_2_0.xsd to be web accessible.

--- src/main/java/se/spacejens/gagror/spring/WelcomeController.java
+++ src/main/java/se/spacejens/gagror/spring/WelcomeController.java
@@ -3,6 +3,10 @@ package se.spacejens.gagror.spring;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import se.spacejens.gagror.LogAwareSupport;
+import se.spacejens.gagror.model.Message;
 
 /**
  * Controller for the welcome page.
@@ -26,7 +31,17 @@ public class WelcomeController extends LogAwareSupport {
 			final HttpServletResponse arg1) {
 		this.getLog().info("Handling request and providing model");
 		final Map<String, Object> model = new HashMap<String, Object>();
-		model.put("brief", "A wild message appears!");
+		final EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("GagrorPersistenceUnit");
+		final EntityManager em = emf.createEntityManager();
+		final EntityTransaction trans = em.getTransaction();
+		trans.begin();
+		Message mess1 = new Message("A persistent message appears!");
+		em.persist(mess1);
+		model.put("brief", mess1.getText() + " (" + mess1.getId() + ")");
+		trans.commit();
+		em.close();
+		emf.close();
 		return new ModelAndView("welcome", "gagror", model);
 	}
 }
