f5966f39c4d661ed7be4035dc1779585644e18ec
Jens Rydholm <spacejens@gmail.com>
2010-11-15 12:50:48 +0100

    Logging in persistence test.

--- src/test/java/se/spacejens/gagror/model/MessagePersistenceTest.java
+++ src/test/java/se/spacejens/gagror/model/MessagePersistenceTest.java
@@ -26,18 +26,26 @@ public class MessagePersistenceTest extends AbstractTest {
 	 */
 	@Test
 	public void testStoreAndRetrieve() throws Exception {
+		this.log.debug("Creating entity manager factory");
 		final EntityManagerFactory emf = Persistence
 				.createEntityManagerFactory("GagrorPersistenceUnit");
+		this.log.debug("Creating entity manager");
 		final EntityManager em = emf.createEntityManager();
+		this.log.debug("Starting transaction");
 		final EntityTransaction trans = em.getTransaction();
 		trans.begin();
+		this.log.debug("Doing some work");
 		Message mess1 = new Message("One");
 		em.persist(mess1);
 		Assert.assertNotNull("Persisted message should have received an ID",
 				mess1.getId());
 		// TODO
+		this.log.debug("Rolling back work");
 		trans.rollback();
+		this.log.debug("Closing entity manager");
 		em.close();
+		this.log.debug("Closing entity manager factory");
 		emf.close();
+		this.log.debug("Done closing");
 	}
 }
