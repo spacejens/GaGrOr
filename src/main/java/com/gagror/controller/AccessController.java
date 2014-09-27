49b980a78093855ccbb713c349ebaaadad1901fa
Jens Rydholm <spacejens@gmail.com>
2014-09-27 10:47:51 +0200

    Login form that checks credentials in DB

--- src/main/java/com/gagror/controller/AccessController.java
+++ src/main/java/com/gagror/controller/AccessController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.gagror.data.account.LoginCredentialsInput;
 import com.gagror.service.accesscontrol.AccessControlService;
@@ -16,8 +17,16 @@ public class AccessController {
 	@Autowired
 	AccessControlService accessControl;
 
-	@RequestMapping("/login")
-	public String login(final Model model) {
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String loginForm(final Model model) {
+		model.addAttribute("loginForm", new LoginCredentialsInput());
+		return "login";
+	}
+
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String loginProcess(final Model model, final LoginCredentialsInput loginForm) {
+		model.addAttribute("loginForm", loginForm);
+		model.addAttribute("loggedInUser", accessControl.logIn(loginForm));
 		return "login";
 	}
 
