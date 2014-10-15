16e0ecb1d62d10b9ff2b870d35e4bd117116ee51
Jens Rydholm <spacejens@gmail.com>
2014-10-15 22:27:00 +0200

    Security roles sorted alphabetically as specified by UserDetails.getAuthorities()

--- src/main/java/com/gagror/data/account/AccountType.java
+++ src/main/java/com/gagror/data/account/AccountType.java
@@ -1,15 +1,17 @@
 package com.gagror.data.account;
 
-import static java.util.Collections.unmodifiableSet;
+import static java.util.Collections.unmodifiableList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 import lombok.Getter;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public enum AccountType {
 
@@ -22,16 +24,20 @@ public enum AccountType {
 	@Getter
 	private final int id;
 
+	/**
+	 * Sorted as needed by {@link UserDetails#getAuthorities()}.
+	 */
 	@Getter
 	private final Collection<GrantedAuthority> authorities;
 
 	private AccountType(final int id, final String... roles) {
 		this.id = id;
-		final Set<GrantedAuthority> tempAuthorities = new HashSet<>();
+		Arrays.sort(roles);
+		final List<GrantedAuthority> tempAuthorities = new ArrayList<>();
 		for(final String role : roles) {
 			tempAuthorities.add(new SimpleGrantedAuthority(role));
 		}
-		authorities = unmodifiableSet(tempAuthorities);
+		authorities = unmodifiableList(tempAuthorities);
 	}
 
 	public static AccountType fromId(final int id) {
