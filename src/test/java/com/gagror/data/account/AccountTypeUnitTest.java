fc24e69121b3a0bdb86b27e00a289fda81030cf8
Jens Rydholm <spacejens@gmail.com>
2014-10-19 13:46:09 +0200

    Unit tests for account type security roles

--- src/test/java/com/gagror/data/account/AccountTypeUnitTest.java
+++ src/test/java/com/gagror/data/account/AccountTypeUnitTest.java
@@ -6,8 +6,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.springframework.security.core.GrantedAuthority;
 
 public class AccountTypeUnitTest {
+
+	// TODO Parameterize or do something else to ensure that all tests exist for all account types
 
 	@Test
 	public void standard_mayEdit() {
@@ -24,7 +27,20 @@ public class AccountTypeUnitTest {
 		assertMayEdit(AccountType.SYSTEM_OWNER, AccountType.STANDARD, AccountType.ADMIN);
 	}
 
-	// TODO Add unit tests for authorities of account types
+	@Test
+	public void standard_authorities() {
+		assertAuthorities(AccountType.STANDARD);
+	}
+
+	@Test
+	public void admin_authorities() {
+		assertAuthorities(AccountType.ADMIN, SecurityRoles.ROLE_ADMIN);
+	}
+
+	@Test
+	public void systemOwner_authorities() {
+		assertAuthorities(AccountType.SYSTEM_OWNER, SecurityRoles.ROLE_ADMIN);
+	}
 
 	private void assertMayEdit(final AccountType actor, final AccountType... expectedMayEdit) {
 		final List<AccountType> expected = Arrays.asList(expectedMayEdit);
@@ -33,6 +49,22 @@ public class AccountTypeUnitTest {
 		}
 		for(final AccountType mayEdit : actor.getMayEdit()) {
 			assertTrue(String.format("Account type %s should not be able to edit %s", actor, mayEdit), expected.contains(mayEdit));
+		}
+	}
+
+	private void assertAuthorities(final AccountType actor, final String... expectedRoles) {
+		final List<String> expected = Arrays.asList(expectedRoles);
+		for(final String exp : expected) {
+			boolean found = false;
+			for(final GrantedAuthority granted : actor.getAuthorities()) {
+				if(exp.equals(granted.getAuthority())) {
+					found = true;
+				}
+			}
+			assertTrue(String.format("Account type %s should be granted %s", actor, exp), found);
+		}
+		for(final GrantedAuthority granted : actor.getAuthorities()) {
+			assertTrue(String.format("Account type %s should not be granted %s", actor, granted.getAuthority()), expected.contains(granted.getAuthority()));
 		}
 	}
 }
