89f3e121d4c173bb395717cc9a2b72585a38bc57
Jens Rydholm <spacejens@gmail.com>
2014-10-19 14:11:07 +0200

    Parameterized AccountTypeUnitTest to make sure it covers all enum instances

---
+++ src/test/java/com/gagror/data/EnumUnitTestSupport.java
@@ -0,0 +1,15 @@
+package com.gagror.data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class EnumUnitTestSupport<E extends Enum<E>> {
+
+	protected static <E extends Enum<E>> Iterable<Object[]> parameterizeForInstances(final Class<E> clazz) {
+		final Set<Object[]> parameters = new HashSet<>();
+		for(final E instance : clazz.getEnumConstants()) {
+			parameters.add(new Object[]{instance.name(), instance});
+		}
+		return parameters;
+	}
+}
--- src/test/java/com/gagror/data/account/AccountTypeUnitTest.java
+++ src/test/java/com/gagror/data/account/AccountTypeUnitTest.java
@@ -3,58 +3,80 @@ package com.gagror.data.account;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.springframework.security.core.GrantedAuthority;
 
-public class AccountTypeUnitTest {
+import com.gagror.data.EnumUnitTestSupport;
 
-	// TODO Parameterize or do something else to ensure that all tests exist for all account types
+@RequiredArgsConstructor
+@RunWith(Parameterized.class)
+public class AccountTypeUnitTest extends EnumUnitTestSupport<AccountType> {
+
+	@RequiredArgsConstructor
+	private enum ExpectedResults {
+		STANDARD(AccountType.STANDARD,
+				Collections.<AccountType>emptyList(),
+				Collections.<String>emptyList()),
+		ADMIN(AccountType.ADMIN,
+				Arrays.asList(AccountType.STANDARD, AccountType.ADMIN),
+				Arrays.asList(SecurityRoles.ROLE_ADMIN)),
+		SYSTEM_OWNER(AccountType.SYSTEM_OWNER,
+				Arrays.asList(AccountType.STANDARD, AccountType.ADMIN),
+				Arrays.asList(SecurityRoles.ROLE_ADMIN));
+
+		private final AccountType accountType;
+		private final List<AccountType> mayEdit;
+		private final List<String> authorities;
+
+		public static ExpectedResults forAccountType(final AccountType type) {
+			for(final ExpectedResults exp : values()) {
+				if(exp.accountType.equals(type)) {
+					return exp;
+				}
+			}
+			throw new IllegalArgumentException(String.format("Test has no expected results for %s", type));
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private final String name;
+
+	private final AccountType accountType;
 
 	@Test
-	public void standard_mayEdit() {
-		assertMayEdit(AccountType.STANDARD);
+	public void getMayEdit() {
+		assertMayEdit(accountType, ExpectedResults.forAccountType(accountType).mayEdit);
 	}
 
 	@Test
-	public void admin_mayEdit() {
-		assertMayEdit(AccountType.ADMIN, AccountType.STANDARD, AccountType.ADMIN);
+	public void getAuthorities() {
+		assertAuthorities(accountType, ExpectedResults.forAccountType(accountType).authorities);
 	}
 
-	@Test
-	public void systemOwner_mayEdit() {
-		assertMayEdit(AccountType.SYSTEM_OWNER, AccountType.STANDARD, AccountType.ADMIN);
+	@Parameters(name="{0}")
+	public static Iterable<Object[]> findInstances() {
+		return parameterizeForInstances(AccountType.class);
 	}
 
-	@Test
-	public void standard_authorities() {
-		assertAuthorities(AccountType.STANDARD);
-	}
-
-	@Test
-	public void admin_authorities() {
-		assertAuthorities(AccountType.ADMIN, SecurityRoles.ROLE_ADMIN);
-	}
-
-	@Test
-	public void systemOwner_authorities() {
-		assertAuthorities(AccountType.SYSTEM_OWNER, SecurityRoles.ROLE_ADMIN);
-	}
-
-	private void assertMayEdit(final AccountType actor, final AccountType... expectedMayEdit) {
-		final List<AccountType> expected = Arrays.asList(expectedMayEdit);
+	private void assertMayEdit(final AccountType actor, final List<AccountType> expectedMayEdit) {
 		for(final AccountType mayEdit : expectedMayEdit) {
 			assertTrue(String.format("Account type %s should be able to edit %s", actor, mayEdit), actor.getMayEdit().contains(mayEdit));
 		}
 		for(final AccountType mayEdit : actor.getMayEdit()) {
-			assertTrue(String.format("Account type %s should not be able to edit %s", actor, mayEdit), expected.contains(mayEdit));
+			assertTrue(String.format("Account type %s should not be able to edit %s", actor, mayEdit), expectedMayEdit.contains(mayEdit));
 		}
 	}
 
-	private void assertAuthorities(final AccountType actor, final String... expectedRoles) {
-		final List<String> expected = Arrays.asList(expectedRoles);
-		for(final String exp : expected) {
+	private void assertAuthorities(final AccountType actor, final List<String> expectedRoles) {
+		for(final String exp : expectedRoles) {
 			boolean found = false;
 			for(final GrantedAuthority granted : actor.getAuthorities()) {
 				if(exp.equals(granted.getAuthority())) {
@@ -64,7 +86,7 @@ public class AccountTypeUnitTest {
 			assertTrue(String.format("Account type %s should be granted %s", actor, exp), found);
 		}
 		for(final GrantedAuthority granted : actor.getAuthorities()) {
-			assertTrue(String.format("Account type %s should not be granted %s", actor, granted.getAuthority()), expected.contains(granted.getAuthority()));
+			assertTrue(String.format("Account type %s should not be granted %s", actor, granted.getAuthority()), expectedRoles.contains(granted.getAuthority()));
 		}
 	}
 }
