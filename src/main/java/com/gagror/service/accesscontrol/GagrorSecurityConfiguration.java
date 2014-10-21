c75eaf4ca8f91ad414d5833983d9d57b852a6a8f
Jens Rydholm <spacejens@gmail.com>
2014-10-21 18:20:08 +0200

    Intercepting privilege evaluator requests to help understand why they fail

--- src/main/java/com/gagror/service/accesscontrol/GagrorSecurityConfiguration.java
+++ src/main/java/com/gagror/service/accesscontrol/GagrorSecurityConfiguration.java
@@ -5,13 +5,14 @@ import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.intercept.AbstractSecurityInterceptor;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.access.WebInvocationPrivilegeEvaluator;
+import org.springframework.security.web.access.DefaultWebInvocationPrivilegeEvaluator;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 /**
@@ -45,26 +46,33 @@ public class GagrorSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(final WebSecurity web) throws Exception {
 		log.info("Configuring GaGrOr application web security");
-		web.privilegeEvaluator(new WebInvocationPrivilegeEvaluator() {
-			// TODO This is the evaluator actually called, since making it return false will disallow all URL checks from Thymeleaf. Why?
-			@Override
-			public boolean isAllowed(final String contextPath, final String uri, final String method,
-					final Authentication authentication) {
-				return false;
-			}
-
-			@Override
-			public boolean isAllowed(final String uri, final Authentication authentication) {
-				return false;
-			}
-		});
 		final HttpSecurity http = getHttp();
 		web.postBuildAction(new Runnable() {
 			// TODO Does this need to be here?
 			@Override
 			public void run() {
 				web.securityInterceptor(http.getSharedObject(FilterSecurityInterceptor.class));
+				web.privilegeEvaluator(new InterceptingPrivilegeEvaluator(http.getSharedObject(FilterSecurityInterceptor.class)));
 			}
 		});
+	}
+
+	private class InterceptingPrivilegeEvaluator extends DefaultWebInvocationPrivilegeEvaluator {
+
+		public InterceptingPrivilegeEvaluator(final AbstractSecurityInterceptor securityInterceptor) {
+			super(securityInterceptor);
+		}
+
+		@Override
+		public boolean isAllowed(final String uri, final Authentication authentication) {
+			log.info(String.format("Checking if URI %s is allowed for %s", uri, authentication));
+			return super.isAllowed(uri, authentication);
+		}
+
+		@Override
+		public boolean isAllowed(final String contextPath, final String uri, final String method, final Authentication authentication) {
+			log.info(String.format("Checking if context %s, method %s, URI %s is allowed for %s", contextPath, method, uri, authentication));
+			return super.isAllowed(contextPath, uri, method, authentication);
+		}
 	}
 }
