56d4704acbebc10c883d76b7034cd08810e5c0c9
Jens Rydholm <spacejens@gmail.com>
2014-10-23 22:23:44 +0200

    AttributeConverters need to implement that interface for JPA implementation to find them

--- src/main/java/com/gagror/data/account/AccountTypeConverter.java
+++ src/main/java/com/gagror/data/account/AccountTypeConverter.java
@@ -1,11 +1,14 @@
 package com.gagror.data.account;
 
+import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 import com.gagror.data.AttributeConverterSupport;
 
 @Converter(autoApply = true)
-public class AccountTypeConverter extends AttributeConverterSupport<Integer, AccountType> {
+public class AccountTypeConverter
+extends AttributeConverterSupport<Integer, AccountType>
+implements AttributeConverter<AccountType, Integer> {
 
 	@Override
 	public AccountType convertToEntityAttribute(final Integer dbData) {
--- src/main/java/com/gagror/data/account/ContactTypeConverter.java
+++ src/main/java/com/gagror/data/account/ContactTypeConverter.java
@@ -1,11 +1,14 @@
 package com.gagror.data.account;
 
+import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 import com.gagror.data.AttributeConverterSupport;
 
 @Converter(autoApply = true)
-public class ContactTypeConverter extends AttributeConverterSupport<Integer, ContactType> {
+public class ContactTypeConverter
+extends AttributeConverterSupport<Integer, ContactType>
+implements AttributeConverter<ContactType, Integer> {
 
 	@Override
 	public ContactType convertToEntityAttribute(final Integer dbData) {
