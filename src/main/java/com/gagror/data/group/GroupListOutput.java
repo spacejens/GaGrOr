dc9018be417ee5c0a7dfb8ebe76b12f5f7449c60
Jens Rydholm <spacejens@gmail.com>
2014-10-31 22:13:09 +0100

    Moved membership counts to only be available in GroupListOutput

--- src/main/java/com/gagror/data/group/GroupListOutput.java
+++ src/main/java/com/gagror/data/group/GroupListOutput.java
@@ -9,8 +9,24 @@ public class GroupListOutput extends GroupReferenceOutput {
 
 	private final MemberType memberType;
 
+	private final int memberCount;
+
+	private final int invitationCount;
+
 	public GroupListOutput(final GroupMemberEntity entity) {
 		super(entity.getGroup());
 		memberType = entity.getMemberType();
+		// Count the number of members and pending membership invitations
+		int tempMemberCount = 0;
+		int tempInvitationCount = 0;
+		for(final GroupMemberEntity member : entity.getGroup().getGroupMemberships()) {
+			if(member.getMemberType().isMember()) {
+				tempMemberCount++;
+			} else if(member.getMemberType().isInvitation()) {
+				tempInvitationCount++;
+			}
+		}
+		memberCount = tempMemberCount;
+		invitationCount = tempInvitationCount;
 	}
 }
--- src/main/java/com/gagror/data/group/GroupReferenceOutput.java
+++ src/main/java/com/gagror/data/group/GroupReferenceOutput.java
@@ -9,26 +9,9 @@ public class GroupReferenceOutput implements Comparable<GroupReferenceOutput> {
 
 	private final String name;
 
-	// TODO Move counts to group list output
-	private final int memberCount;
-
-	private final int invitationCount;
-
 	public GroupReferenceOutput(final GroupEntity entity) {
 		id = entity.getId();
 		name = entity.getName();
-		// Count the number of members and pending membership invitations
-		int tempMemberCount = 0;
-		int tempInvitationCount = 0;
-		for(final GroupMemberEntity member : entity.getGroupMemberships()) {
-			if(member.getMemberType().isMember()) {
-				tempMemberCount++;
-			} else if(member.getMemberType().isInvitation()) {
-				tempInvitationCount++;
-			}
-		}
-		memberCount = tempMemberCount;
-		invitationCount = tempInvitationCount;
 	}
 
 	@Override
