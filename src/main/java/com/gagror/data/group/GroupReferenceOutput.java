ca58d6ba6a9210f036faa42ea008177b7aa103ef
Jens Rydholm <spacejens@gmail.com>
2014-10-31 18:46:24 +0100

    Groups listing shows member and invitation counts

--- src/main/java/com/gagror/data/group/GroupReferenceOutput.java
+++ src/main/java/com/gagror/data/group/GroupReferenceOutput.java
@@ -9,9 +9,25 @@ public class GroupReferenceOutput implements Comparable<GroupReferenceOutput> {
 
 	private final String name;
 
+	private final int memberCount;
+
+	private final int invitationCount;
+
 	public GroupReferenceOutput(final GroupEntity entity) {
 		id = entity.getId();
 		name = entity.getName();
+		// Count the number of members and pending membership invitations
+		int tempMemberCount = 0;
+		int tempInvitationCount = 0;
+		for(final GroupMemberEntity member : entity.getGroupMemberships()) {
+			if(member.getMemberType().isMember()) {
+				tempMemberCount++;
+			} else if(member.getMemberType().isInvitation()) {
+				tempInvitationCount++;
+			}
+		}
+		memberCount = tempMemberCount;
+		invitationCount = tempInvitationCount;
 	}
 
 	@Override
