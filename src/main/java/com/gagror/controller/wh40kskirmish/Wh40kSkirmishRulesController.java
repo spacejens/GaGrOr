788a8d3fc1f3d59fee7323c59427ff6f3b3e047b
Jens Rydholm <spacejens@gmail.com>
2014-11-23 20:14:35 +0100

    Added empty pages for creating and listing gang types

--- src/main/java/com/gagror/controller/wh40kskirmish/Wh40kSkirmishRulesController.java
+++ src/main/java/com/gagror/controller/wh40kskirmish/Wh40kSkirmishRulesController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.gagror.controller.AbstractController;
 import com.gagror.service.social.GroupService;
@@ -24,7 +25,49 @@ public class Wh40kSkirmishRulesController extends AbstractController {
 	@RequestMapping("/view/{" + ATTR_GROUP_ID + "}")
 	public String viewRules(@PathVariable(ATTR_GROUP_ID) final Long groupId, final Model model) {
 		log.info(String.format("Viewing rules for group %d", groupId));
+		// TODO Use different data to view basic rules
 		model.addAttribute("group", groupService.viewGroup(groupId));
+		// TODO Make initial territory allocation for gangs configurable
+		// TODO Make initial money per gang configurable
+		// TODO Make name of currency in game configurable
 		return "wh40kskirmish/rules_view";
 	}
+
+	// TODO Add page to edit basic rules
+
+	@PreAuthorize(MAY_VIEW_GROUP)
+	@RequestMapping("/gangtypes/list/{" + ATTR_GROUP_ID + "}")
+	public String listGangTypes(@PathVariable(ATTR_GROUP_ID) final Long groupId, final Model model) {
+		log.info(String.format("Viewing gang types for group %d", groupId));
+		// TODO Use different data and HTML to list gang types
+		model.addAttribute("group", groupService.viewGroup(groupId));
+		return "wh40kskirmish/gangtypes_list";
+	}
+
+	@PreAuthorize(MAY_ADMIN_GROUP)
+	@RequestMapping(value="/gangtypes/create/{" + ATTR_GROUP_ID + "}", method=RequestMethod.GET)
+	public String createGangTypeForm(@PathVariable(ATTR_GROUP_ID) final Long groupId, final Model model) {
+		log.info(String.format("Viewing create gang type form for group %d", groupId));
+		// TODO Use different data and HTML to create gang types
+		model.addAttribute("group", groupService.viewGroup(groupId));
+		return "wh40kskirmish/gangtypes_create";
+	}
+
+	// TODO Add logic to handle posting of create gang type form
+
+	// TODO Add page to view single gang type
+
+	// TODO Add page to edit gang types
+
+	// TODO Add page to view skills
+
+	// TODO Add page to edit skills
+
+	// TODO Add page to view equipment
+
+	// TODO Add page to edit equipment
+
+	// TODO Add page to view territories
+
+	// TODO Add page to edit territories
 }
