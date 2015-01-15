585f5137088ed64034755ac0c3bdb567fb07d5a7
Jens Rydholm <spacejens@gmail.com>
2015-01-15 21:22:44 +0100

    Entity mapping for fighter types

--- src/main/java/com/gagror/data/wh40kskirmish/Wh40kSkirmishFighterTypeEntity.java
+++ src/main/java/com/gagror/data/wh40kskirmish/Wh40kSkirmishFighterTypeEntity.java
@@ -1,12 +1,8 @@
 package com.gagror.data.wh40kskirmish;
 
-import java.util.Set;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -18,21 +14,17 @@ import com.gagror.data.AbstractEditableNamedEntity;
 @NoArgsConstructor
 @ToString(of={}, callSuper=true)
 @Entity
-@Table(name="wh40ksk_race")
-public class Wh40kSkirmishRaceEntity extends AbstractEditableNamedEntity {
+@Table(name="wh40ksk_fightertype")
+public class Wh40kSkirmishFighterTypeEntity extends AbstractEditableNamedEntity {
 
 	@ManyToOne(optional=false)
 	@JoinColumn(nullable=false, insertable=true, updatable=false)
 	@Getter
-	private Wh40kSkirmishGangTypeEntity gangType;
+	private Wh40kSkirmishRaceEntity race;
 
-	@OneToMany(mappedBy="race", fetch=FetchType.LAZY)
-	@Getter
-	private Set<Wh40kSkirmishFighterTypeEntity> fighterTypes;
-
-	public Wh40kSkirmishRaceEntity(final Wh40kSkirmishGangTypeEntity gangType) {
-		this.gangType = gangType;
+	public Wh40kSkirmishFighterTypeEntity(final Wh40kSkirmishRaceEntity race) {
+		this.race = race;;
 		// Add the new entity to the referencing collection
-		gangType.getRaces().add(this);
+		race.getFighterTypes().add(this);
 	}
 }
