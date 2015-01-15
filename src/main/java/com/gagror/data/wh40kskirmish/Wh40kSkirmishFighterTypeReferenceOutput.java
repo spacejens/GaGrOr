d39180e67cb1bd8e6974df1670669949acba1549
Jens Rydholm <spacejens@gmail.com>
2015-01-15 21:37:02 +0100

    List fighter types on rules overview page

---
+++ src/main/java/com/gagror/data/wh40kskirmish/Wh40kSkirmishFighterTypeReferenceOutput.java
@@ -0,0 +1,11 @@
+package com.gagror.data.wh40kskirmish;
+
+import com.gagror.data.AbstractEditableNamedEntityOutput;
+
+public class Wh40kSkirmishFighterTypeReferenceOutput
+extends AbstractEditableNamedEntityOutput {
+
+	public Wh40kSkirmishFighterTypeReferenceOutput(final Wh40kSkirmishFighterTypeEntity entity) {
+		super(entity);
+	}
+}
--- src/main/java/com/gagror/data/wh40kskirmish/Wh40kSkirmishRaceListChildrenOutput.java
+++ src/main/java/com/gagror/data/wh40kskirmish/Wh40kSkirmishRaceListChildrenOutput.java
@@ -1,11 +1,25 @@
 package com.gagror.data.wh40kskirmish;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import lombok.Getter;
+
 public class Wh40kSkirmishRaceListChildrenOutput
 extends Wh40kSkirmishRaceReferenceOutput {
+
+	@Getter
+	private final List<Wh40kSkirmishFighterTypeReferenceOutput> fighterTypes;
 
 	public Wh40kSkirmishRaceListChildrenOutput(final Wh40kSkirmishRaceEntity entity) {
 		super(entity);
 		// Extract a sorted list of fighter types
-		// TODO Sorted list of fighter types of race in output object
+		final List<Wh40kSkirmishFighterTypeReferenceOutput> fighterTypes = new ArrayList<>();
+		for(final Wh40kSkirmishFighterTypeEntity fighterType : entity.getFighterTypes()) {
+			fighterTypes.add(new Wh40kSkirmishFighterTypeReferenceOutput(fighterType));
+		}
+		Collections.sort(fighterTypes);
+		this.fighterTypes = fighterTypes;
 	}
 }
