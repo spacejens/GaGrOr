f72da8406fe638a2dad17dc3817d1360796d9fad
Jens Rydholm <spacejens@gmail.com>
2015-01-15 21:06:57 +0100

    Corrected argument name in faction repository

--- src/main/java/com/gagror/data/wh40kskirmish/Wh40kSkirmishFactionRepository.java
+++ src/main/java/com/gagror/data/wh40kskirmish/Wh40kSkirmishFactionRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.Repository;
 
 public interface Wh40kSkirmishFactionRepository extends Repository<Wh40kSkirmishFactionEntity, Long> {
 
-	Wh40kSkirmishFactionEntity save(final Wh40kSkirmishFactionEntity gangType);
+	Wh40kSkirmishFactionEntity save(final Wh40kSkirmishFactionEntity faction);
 }
