9f66ad7c84e9cd25aa475476db3bc5c639f8c02f
Jens Rydholm <spacejens@gmail.com>
2015-01-17 18:36:29 +0100

    Removed some unnecessary code generation

--- src/main/java/com/gagror/data/account/SecurityUser.java
+++ src/main/java/com/gagror/data/account/SecurityUser.java
@@ -2,20 +2,32 @@ package com.gagror.data.account;
 
 import java.util.Collection;
 
-import lombok.Data;
+import lombok.Getter;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@Data
 public class SecurityUser implements UserDetails {
 
+	@Getter
 	private final String password;
+
+	@Getter
 	private final String username;
+
+	@Getter
 	private final boolean accountNonExpired;
+
+	@Getter
 	private final boolean accountNonLocked;
+
+	@Getter
 	private final boolean credentialsNonExpired;
+
+	@Getter
 	private final boolean enabled;
+
+	@Getter
 	private final Collection<GrantedAuthority> authorities;
 
 	public SecurityUser(final AccountEntity account) {
