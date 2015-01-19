62e6683ef36ed83dade100aeea500dde58bd6980
Jens Rydholm <spacejens@gmail.com>
2015-01-19 19:16:03 +0100

    List territory types for each category on rules overview page

---
+++ src/main/java/com/gagror/data/wh40kskirmish/rules/territory/Wh40kSkirmishTerritoryTypeOutput.java
@@ -0,0 +1,8 @@
+package com.gagror.data.wh40kskirmish.rules.territory;
+
+public class Wh40kSkirmishTerritoryTypeOutput extends Wh40kSkirmishTerritoryTypeReferenceOutput {
+
+	public Wh40kSkirmishTerritoryTypeOutput(final Wh40kSkirmishTerritoryTypeEntity entity) {
+		super(entity);
+	}
+}
