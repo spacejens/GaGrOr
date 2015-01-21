1764efd28678fd8f9cfe319a2053d4a7ca0d41e7
Jens Rydholm <spacejens@gmail.com>
2015-01-21 09:41:58 +0100

    Fixed test bug, didn't load all classes for code coverage

--- src/test/java/com/gagror/LoadAllClassesInTest.java
+++ src/test/java/com/gagror/LoadAllClassesInTest.java
@@ -1,10 +1,12 @@
 package com.gagror;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
 import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
 
 public class LoadAllClassesInTest {
 
@@ -12,9 +14,14 @@ public class LoadAllClassesInTest {
 	public void loadAllClasses() {
 		// Load all classes to ensure that e.g. code coverage tools see them
 		final List<String> allClasses = new ArrayList<>();
-		final Reflections reflections = new Reflections("com.gagror");
+		final Reflections reflections = new Reflections("com.gagror", new SubTypesScanner(false));
 		for(final Class<?> clazz : reflections.getSubTypesOf(Object.class)) {
 			allClasses.add(clazz.getName());
+		}
+		// Print all class names in order
+		Collections.sort(allClasses);
+		for(final String clazz : allClasses) {
+			System.out.println("Found class: " + clazz);
 		}
 	}
 }
