0eed35ab19078248bc3dda0edfff15f858fe429b
Jens Rydholm <spacejens@gmail.com>
2015-01-24 13:22:21 +0100

    Show faction in gang list on view group page

--- src/main/java/com/gagror/data/wh40kskirmish/gangs/Wh40kSkirmishGangListOutput.java
+++ src/main/java/com/gagror/data/wh40kskirmish/gangs/Wh40kSkirmishGangListOutput.java
@@ -1,12 +1,18 @@
 package com.gagror.data.wh40kskirmish.gangs;
 
+import lombok.Getter;
+
+import com.gagror.data.wh40kskirmish.rules.gangs.Wh40kSkirmishFactionReferenceOutput;
+
 public class Wh40kSkirmishGangListOutput extends Wh40kSkirmishGangReferenceOutput {
 
-	// TODO Show faction in gang listing
+	@Getter
+	private final Wh40kSkirmishFactionReferenceOutput faction;
 
 	// TODO Show player in gang listing
 
 	public Wh40kSkirmishGangListOutput(final Wh40kSkirmishGangEntity entity) {
 		super(entity);
+		faction = new Wh40kSkirmishFactionReferenceOutput(entity.getFaction());
 	}
 }
