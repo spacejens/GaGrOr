b6d442e9d37ec0bb360f89493c2d69cdb93d086e
Jens Rydholm <spacejens@gmail.com>
2015-01-24 13:27:09 +0100

    Show player in gang list on view group page

--- src/main/java/com/gagror/data/wh40kskirmish/gangs/Wh40kSkirmishGangListOutput.java
+++ src/main/java/com/gagror/data/wh40kskirmish/gangs/Wh40kSkirmishGangListOutput.java
@@ -2,6 +2,7 @@ package com.gagror.data.wh40kskirmish.gangs;
 
 import lombok.Getter;
 
+import com.gagror.data.account.AccountReferenceOutput;
 import com.gagror.data.wh40kskirmish.rules.gangs.Wh40kSkirmishFactionReferenceOutput;
 
 public class Wh40kSkirmishGangListOutput extends Wh40kSkirmishGangReferenceOutput {
@@ -9,10 +10,12 @@ public class Wh40kSkirmishGangListOutput extends Wh40kSkirmishGangReferenceOutpu
 	@Getter
 	private final Wh40kSkirmishFactionReferenceOutput faction;
 
-	// TODO Show player in gang listing
+	@Getter
+	private final AccountReferenceOutput player;
 
 	public Wh40kSkirmishGangListOutput(final Wh40kSkirmishGangEntity entity) {
 		super(entity);
 		faction = new Wh40kSkirmishFactionReferenceOutput(entity.getFaction());
+		player = new AccountReferenceOutput(entity.getPlayer());
 	}
 }
