d642adefadaa807b5eb3900293649590841f7640
Jens Rydholm <spacejens@gmail.com>
2015-02-08 09:20:10 +0100

    Account needs to be active group member or group owner to get player permission

--- src/main/java/com/gagror/service/accesscontrol/AbstractPermissionPlayer.java
+++ src/main/java/com/gagror/service/accesscontrol/AbstractPermissionPlayer.java
@@ -4,6 +4,7 @@ import lombok.extern.apachecommons.CommonsLog;
 
 import com.gagror.data.Identifiable;
 import com.gagror.data.account.AccountEntity;
+import com.gagror.data.group.GroupMemberEntity;
 import com.gagror.data.group.GroupOwned;
 import com.gagror.data.group.PlayerOwned;
 
@@ -18,13 +19,27 @@ extends AbstractGagrorPermissionLongId<E> {
 	@Override
 	public final boolean hasPermission(final Long id, final AccountEntity account) {
 		final E target = loadTarget(id);
+		// Find the group membership of the account
+		GroupMemberEntity accountMembership = null;
+		for(final GroupMemberEntity groupMembership : target.getGroup().getGroupMemberships()) {
+			if(groupMembership.getAccount().equals(account)) {
+				accountMembership = groupMembership;
+			}
+		}
+		if(null == accountMembership || ! accountMembership.getMemberType().isMember()) {
+			log.debug(String.format("Account %s is not a member of group of %s", account, target));
+			return false;
+		}
 		// Allow if player
 		if(target.getPlayer().equals(account)) {
 			log.debug(String.format("Account %s is player of %s", account, target));
 			return true;
 		}
-		// TODO Account should only count as player if currently a member of the group
-		// TODO Group owners should be granted player permission for all targets within the group
+		// Allow if group owner
+		if(accountMembership.getMemberType().isOwner()) {
+			log.debug(String.format("Account %s is group owner for group of %s", account, target));
+			return true;
+		}
 		// Disallow for everyone else
 		return false;
 	}
--- src/test/java/com/gagror/service/wh40kskirmish/Wh40kSkirmishPermissionGangPlayerUnitTest.java
+++ src/test/java/com/gagror/service/wh40kskirmish/Wh40kSkirmishPermissionGangPlayerUnitTest.java
@@ -3,7 +3,10 @@ package com.gagror.service.wh40kskirmish;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +16,9 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import com.gagror.data.DataNotFoundException;
 import com.gagror.data.account.AccountEntity;
+import com.gagror.data.group.GroupEntity;
+import com.gagror.data.group.GroupMemberEntity;
+import com.gagror.data.group.MemberType;
 import com.gagror.data.wh40kskirmish.gangs.GangEntity;
 import com.gagror.data.wh40kskirmish.gangs.GangRepository;
 
@@ -21,7 +27,8 @@ public class Wh40kSkirmishPermissionGangPlayerUnitTest {
 
 	private static final Long GANG_ID = 12L;
 	private static final Long ACCOUNT_ID_PLAYER = 34L;
-	private static final Long ACCOUNT_ID_OTHER = 56L;
+	private static final Long ACCOUNT_ID_GROUP_OWNER = 56L;
+	private static final Long ACCOUNT_ID_OTHER = 78L;
 
 	Wh40kSkirmishPermissionGangPlayer instance;
 
@@ -32,7 +39,13 @@ public class Wh40kSkirmishPermissionGangPlayerUnitTest {
 	GangEntity gang;
 
 	@Mock
+	GroupEntity group;
+
+	@Mock
 	AccountEntity player;
+
+	@Mock
+	AccountEntity groupOwner;
 
 	@Mock
 	AccountEntity anotherAccount;
@@ -40,6 +53,23 @@ public class Wh40kSkirmishPermissionGangPlayerUnitTest {
 	@Test
 	public void hasPermission_player_ok() {
 		assertTrue("Player should have permission", instance.hasPermission(GANG_ID, player));
+	}
+
+	@Test
+	public void hasPermission_playerNoLongerMember_notOk() {
+		setupAccount(player, ACCOUNT_ID_PLAYER, null);
+		assertFalse("Player should not have permission when no longer member", instance.hasPermission(GANG_ID, player));
+	}
+
+	@Test
+	public void hasPermission_playerOnlyInvited_notOk() {
+		setupAccount(player, ACCOUNT_ID_PLAYER, MemberType.INVITED);
+		assertFalse("Player should not have permission when only invited", instance.hasPermission(GANG_ID, player));
+	}
+
+	@Test
+	public void hasPermission_groupOwner_ok() {
+		assertTrue("Group owner should have permission", instance.hasPermission(GANG_ID, groupOwner));
 	}
 
 	@Test
@@ -54,16 +84,44 @@ public class Wh40kSkirmishPermissionGangPlayerUnitTest {
 	}
 
 	@Before
-	public void setupAccounts() {
-		when(player.getId()).thenReturn(ACCOUNT_ID_PLAYER);
-		when(anotherAccount.getId()).thenReturn(ACCOUNT_ID_OTHER);
-	}
-
-	@Before
-	public void setupGang() {
+	public void setup() {
 		when(gang.getId()).thenReturn(GANG_ID);
 		when(gang.getPlayer()).thenReturn(player);
+		when(gang.getGroup()).thenReturn(group);
 		when(gangRepository.load(GANG_ID)).thenReturn(gang);
+		when(group.getGroupMemberships()).thenReturn(new HashSet<GroupMemberEntity>());
+		setupAccount(player, ACCOUNT_ID_PLAYER, MemberType.MEMBER);
+		setupAccount(groupOwner, ACCOUNT_ID_GROUP_OWNER, MemberType.OWNER);
+		setupAccount(anotherAccount, ACCOUNT_ID_OTHER, MemberType.MEMBER);
+	}
+
+	private void setupAccount(final AccountEntity account, final Long accountId, final MemberType memberType) {
+		when(account.getId()).thenReturn(accountId);
+		// Try to find an existing membership to change
+		GroupMemberEntity membership = null;
+		for(final GroupMemberEntity existingMembership : group.getGroupMemberships()) {
+			if(existingMembership.getAccount().equals(account)) {
+				membership = existingMembership;
+			}
+		}
+		// If account should not be member, set that up
+		if(null == memberType) {
+			// Remove membership if it already exists
+			if(null != membership) {
+				group.getGroupMemberships().remove(membership);
+			}
+			return;
+		}
+		// Create a new membership if needed
+		if(null == membership) {
+			membership = mock(GroupMemberEntity.class);
+			when(membership.getId()).thenReturn(accountId); // Reuse the account ID to be unique
+			when(membership.getGroup()).thenReturn(group);
+			when(membership.getAccount()).thenReturn(account);
+			group.getGroupMemberships().add(membership);
+		}
+		// Set the membership level
+		when(membership.getMemberType()).thenReturn(memberType);
 	}
 
 	@Before
