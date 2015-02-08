8e2a111618df424ff284240711adbb4fa558464f
Jens Rydholm <spacejens@gmail.com>
2015-02-08 10:12:00 +0100

    Entity mapping for gang fighters

--- src/main/java/com/gagror/data/wh40kskirmish/gangs/FighterEntity.java
+++ src/main/java/com/gagror/data/wh40kskirmish/gangs/FighterEntity.java
@@ -1,18 +1,12 @@
 package com.gagror.data.wh40kskirmish.gangs;
 
-import java.util.Set;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 import com.gagror.data.AbstractEditableNamedEntity;
@@ -20,42 +14,38 @@ import com.gagror.data.account.AccountEntity;
 import com.gagror.data.group.GroupEntity;
 import com.gagror.data.group.GroupOwned;
 import com.gagror.data.group.PlayerOwned;
-import com.gagror.data.wh40kskirmish.rules.gangs.FactionEntity;
+import com.gagror.data.wh40kskirmish.rules.gangs.FighterTypeEntity;
 
 @NoArgsConstructor
 @ToString(of={}, callSuper=true)
 @Entity
-@Table(name="wh40ksk_gang")
-public class GangEntity extends AbstractEditableNamedEntity implements GroupOwned, PlayerOwned {
+@Table(name="wh40ksk_fighter")
+public class FighterEntity extends AbstractEditableNamedEntity implements GroupOwned, PlayerOwned {
 
 	@ManyToOne(optional=false)
 	@JoinColumn(nullable=false, insertable=true, updatable=false)
 	@Getter
-	private FactionEntity faction;
+	private GangEntity gang;
 
 	@ManyToOne(optional=false)
 	@JoinColumn(nullable=false, insertable=true, updatable=false)
 	@Getter
-	private AccountEntity player;
+	private FighterTypeEntity fighterType;
 
-	@OneToMany(mappedBy="gang", fetch=FetchType.LAZY)
-	@Getter
-	private Set<FighterEntity> fighters;
-
-	@Column(nullable = false, insertable = true, updatable = true)
-	@Getter
-	@Setter
-	private int money;
-
-	public GangEntity(final FactionEntity faction, final AccountEntity player) {
-		this.faction = faction;
-		this.player = player;
+	public FighterEntity(final GangEntity gang, final FighterTypeEntity fighterType) {
+		this.gang = gang;
+		this.fighterType = fighterType;
 		// Add the new entity to the referencing collection
-		faction.getGangs().add(this);
+		gang.getFighters().add(this);
+	}
+
+	@Override
+	public AccountEntity getPlayer() {
+		return getGang().getPlayer();
 	}
 
 	@Override
 	public GroupEntity getGroup() {
-		return getFaction().getGroup();
+		return getGang().getGroup();
 	}
 }
