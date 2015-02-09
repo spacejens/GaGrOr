cff286828dc1d7c39369e31b6a0aa31585e12b20
Jens Rydholm <spacejens@gmail.com>
2015-02-09 21:28:29 +0100

    Removed GangOutputFields that were accidentally duplicated from superclass

--- src/main/java/com/gagror/data/wh40kskirmish/gangs/GangOutput.java
+++ src/main/java/com/gagror/data/wh40kskirmish/gangs/GangOutput.java
@@ -2,10 +2,7 @@ package com.gagror.data.wh40kskirmish.gangs;
 
 import lombok.Getter;
 
-import com.gagror.data.account.AccountReferenceOutput;
 import com.gagror.data.wh40kskirmish.rules.RulesOutput;
-import com.gagror.data.wh40kskirmish.rules.gangs.FactionReferenceOutput;
-import com.gagror.data.wh40kskirmish.rules.gangs.GangTypeReferenceOutput;
 
 public class GangOutput extends GangListOutput {
 
@@ -13,23 +10,11 @@ public class GangOutput extends GangListOutput {
 	private final RulesOutput rules;
 
 	@Getter
-	private final GangTypeReferenceOutput gangType;
-
-	@Getter
-	private final FactionReferenceOutput faction;
-
-	@Getter
-	private final AccountReferenceOutput player;
-
-	@Getter
 	private final int money;
 
 	public GangOutput(final GangEntity entity, final RulesOutput rules) {
 		super(entity);
 		this.rules = rules;
-		gangType = new GangTypeReferenceOutput(entity.getFaction().getGangType());
-		faction = new FactionReferenceOutput(entity.getFaction());
-		player = new AccountReferenceOutput(entity.getPlayer());
 		money = entity.getMoney();
 	}
 }
