43fec094fd1e27fee179d54eef25d4169fe0b9b0
Jens Rydholm <spacejens@gmail.com>
2015-02-15 12:38:04 +0100

    TODO avoid train wrecks in Thymeleaf HTML

--- src/main/java/com/gagror/data/Output.java
+++ src/main/java/com/gagror/data/Output.java
@@ -2,4 +2,5 @@ package com.gagror.data;
 
 public interface Output {
 
+	// TODO Create page data objects to avoid train wrecks in Thymeleaf HTML, found by this regexp \$\{[^}]*\.[a-z]*\.[a-z]*\.[^}]*\}
 }
