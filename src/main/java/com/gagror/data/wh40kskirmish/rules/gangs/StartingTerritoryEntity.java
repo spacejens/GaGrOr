d356f4d1e50c08a5cdd9287eeca01bef17a56afe
Jens Rydholm <spacejens@gmail.com>
2015-02-22 13:24:10 +0100

    Starting territories are group owned data

--- src/main/java/com/gagror/data/wh40kskirmish/rules/gangs/StartingTerritoryEntity.java
+++ src/main/java/com/gagror/data/wh40kskirmish/rules/gangs/StartingTerritoryEntity.java
@@ -10,13 +10,15 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import com.gagror.data.AbstractEditableEntity;
+import com.gagror.data.group.GroupEntity;
+import com.gagror.data.group.GroupOwned;
 import com.gagror.data.wh40kskirmish.rules.territory.TerritoryTypeEntity;
 
 @NoArgsConstructor
 @ToString(of={}, callSuper=true)
 @Entity
 @Table(name="wh40ksk_startingterritory")
-public class StartingTerritoryEntity extends AbstractEditableEntity {
+public class StartingTerritoryEntity extends AbstractEditableEntity implements GroupOwned {
 
 	@ManyToOne(optional=false)
 	@JoinColumn(nullable=false, insertable=true, updatable=false)
@@ -33,5 +35,10 @@ public class StartingTerritoryEntity extends AbstractEditableEntity {
 		this.territoryType = territoryType;
 		// Add the new entity to the referencing collection
 		gangType.getStartingTerritories().add(this);
+	}
+
+	@Override
+	public GroupEntity getGroup() {
+		return getGangType().getGroup();
 	}
 }
